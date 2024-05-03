c0d89b206e36bec6ae77d54149d7bdd014a4d18a
Fanrito <2902566341@qq.com>
2024-05-04 01:17:50 +0800

    feat: 新增对yml和yaml的支持（仅消费者端可用，服务端暂不支持）

--- example-consumer/src/main/java/com/fanrito/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/fanrito/example/consumer/ConsumerExample.java
@@ -8,7 +8,7 @@ import com.fanrito.fanrpc.utils.ConfigUtils;
 
 public class ConsumerExample {
     public static void main(String[] args) {
-        RpcConfig rpcConfig = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        RpcConfig rpcConfig = ConfigUtils.loadConfig(RpcConfig.class, "rpc", "yml");
         UserService userService = ServiceProxyFactory.getProxy(UserService.class, rpcConfig);
         User user = new User();
         user.setName("fanrito");
--- fan-rpc-core/src/main/java/com/fanrito/fanrpc/utils/ConfigUtils.java
+++ fan-rpc-core/src/main/java/com/fanrito/fanrpc/utils/ConfigUtils.java
@@ -3,12 +3,20 @@ package com.fanrito.fanrpc.utils;
 
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.setting.dialect.Props;
+import com.fanrito.fanrpc.config.RpcConfig;
 import com.fanrito.fanrpc.constant.RpcConstant;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * 配置工具类
  */
 public class ConfigUtils {
+
 
     /**
      * 加载配置文件
@@ -22,7 +30,18 @@ public class ConfigUtils {
     }
 
     /**
-     * 加载配置文件(支持多种环境)
+     * 加载配置文件(需要指定格式，支持properties和yml)
+     * @param tClass
+     * @param prefix
+     * @return
+     * @param <T>
+     */
+    public static <T> T loadConfig(Class<T> tClass, String prefix, String format) {
+        return loadConfig(tClass, prefix, "", format);
+    }
+
+    /**
+     * 加载配置文件(需要指定环境和格式，支持properties和yml)
      * @param tClass
      * @param prefix
      * @param environment
@@ -35,7 +54,36 @@ public class ConfigUtils {
             configFileBuffer.append("-").append(environment);
         }
         configFileBuffer.append("." + format);
-        Props props = new Props(configFileBuffer.toString());
-        return props.toBean(tClass, prefix);
+        if (format.equals("properties")) {
+            Props props = new Props(configFileBuffer.toString());
+            return props.toBean(tClass, prefix);
+        } else if (format.equals("yml") || format.equals("yaml")) {
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+            // 读取 YAML 文件
+            try {
+                Map<String, Object> data = mapper.readValue(ConfigUtils.class.getClassLoader().getResource(configFileBuffer.toString()), Map.class);
+                // 处理 YAML 数据
+                System.out.println(data);
+                for (Map.Entry<String, Object> entry : data.entrySet()) {
+                    String key = entry.getKey();
+                    if (key.startsWith(prefix)) {
+                        // 提取对象并转换为 RpcConfig 类
+                        Object value = entry.getValue();
+                        if (value instanceof Map) {
+                            Map<String, Object> configData = (Map<String, Object>) value;
+                            T config = mapper.convertValue(configData, tClass);
+                            if (config != null) {
+                                return config;
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
 }
