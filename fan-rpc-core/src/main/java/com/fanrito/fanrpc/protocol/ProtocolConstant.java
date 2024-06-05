db261aced14b8983020ab688326942c9db17ce9e
Fanrito <2902566341@qq.com>
2024-06-05 23:07:35 +0800

    feat: 实现自定义协议

---
+++ fan-rpc-core/src/main/java/com/fanrito/fanrpc/protocol/ProtocolConstant.java
@@ -0,0 +1,23 @@
+package com.fanrito.fanrpc.protocol;
+
+/**
+ * 协议常量
+ *
+ */
+public interface ProtocolConstant {
+
+    /**
+     * 消息头长度
+     */
+    int MESSAGE_HEADER_LENGTH = 17;
+
+    /**
+     * 协议魔数
+     */
+    byte PROTOCOL_MAGIC = 0x1;
+
+    /**
+     * 协议版本号
+     */
+    byte PROTOCOL_VERSION = 0x1;
+}
